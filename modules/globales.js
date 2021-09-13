a3043b3324db3905d796a4ca125f2e4cd3cd8bb6
drolejan <drolejan@gmail.com>
2021-09-13 16:05:52 -0500

    Completado hasta lección 14

---
+++ modules/globales.js
@@ -0,0 +1,18 @@
+//console.log(setInterval);
+// let i=0;
+// let intervalo=setInterval(function(){
+//     console.log('Hola');
+//     if(i===3){
+//         clearInterval(intervalo);
+//     }
+//     i++;
+// },1000);
+
+// setImmediate(function(){
+//     console.log('Hola');
+// })
+
+//console.log(__filename);
+
+global.miVariable = 'unValor';
+console.log(miVariable);
